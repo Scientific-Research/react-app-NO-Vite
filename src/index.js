185665da8f0582ed47ea88d69d86968b0681a0b5
Maximilian <scientificresearch309@gmail.com>
2023-07-31 10:33:21 +0200

    Hello World example explained!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-import React from "react";
-import ReactDom from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+const element = <h1>Hello World</h1>;
+console.log(element);
+
+ReactDOM.render(element, document.getElementById('root'));
