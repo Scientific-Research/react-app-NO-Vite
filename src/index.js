8a2ad3cdb3cd4ac46b7506e652bc36389b268315
Maximilian <scientificresearch309@gmail.com>
2023-08-01 21:46:26 +0200

    difference between var let and const

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1>Hello World</h1>;
-console.log(element);
+// const element = <h1>Hello World</h1>;
+// console.log(element);
 
-ReactDOM.render(element, document.getElementById('root'));
+// ReactDOM.render(element, document.getElementById('root'));
+
+// var => function
+// let => Block
+// const => Block
+
+// let element = 0;
+const sayHello = () => {
+	for (let i = 0; i < 5; i++) {
+		console.log(i);
+		// element = <h1>{i}</h1>;
+		ReactDOM.render(i, document.getElementById('root'));
+	}
+};
+sayHello();
