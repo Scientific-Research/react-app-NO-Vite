24d7a258f455663bb2e26d50e0f979690ca6755e
Maximilian <scientificresearch309@gmail.com>
2023-08-02 22:38:25 +0200

    Object Destructuring

---
+++ src/index Object Destructuring.js
@@ -0,0 +1,13 @@
+const address = {
+	street: '',
+	city: '',
+	country: '',
+};
+
+// const street = address.street;
+// const city = address.city;
+// const country = address.country;
+
+// const { street, city, country } = address;
+// st: is an alias for street(instead of using street, we can use st as abbreviation)
+const { street: st } = address;
